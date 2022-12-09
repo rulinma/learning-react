3dc38350b18f327dbe309db5fe0af6e57cf12a26
rulinma <rulinma@gmail.com>
2022-12-09 22:31:14 +0800

    添加typescript支持

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
-import { Button, DatePicker } from 'antd';
+import { Button, DatePicker, Table } from 'antd';
 
 import './App.css';
 
 function App() {
+  const dataSource = [
+    {
+      key: '1',
+      name: '胡彦斌',
+      age: 32,
+      address: '西湖区湖底公园1号',
+    },
+    {
+      key: '2',
+      name: '胡彦祖',
+      age: 42,
+      address: '西湖区湖底公园1号',
+    },
+  ];
+  
+  const columns = [
+    {
+      title: '姓名',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: '年龄',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: '住址',
+      dataIndex: 'address',
+      key: 'address',
+    },
+  ];
+  
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -12,6 +46,10 @@ function App() {
       <div className="App-button">
         <Button type="primary">PRESS ME</Button>
         <DatePicker placeholder="select date" />
+      </div>
+
+      <div>
+        <Table dataSource={dataSource} columns={columns} />;
       </div>
 
     </div>
